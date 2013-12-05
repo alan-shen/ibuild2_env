b605b7b1f123954763437c2a370b47aa89ae93da
b645 <b645@borqs.com>
2013-12-05 16:06:00 +0800

    Modify some scripts

--- bin/ducati.sh
+++ bin/ducati.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-BASE_DIR=/home/b645/ibuild2/devr4tw32
-PROD_DIR=/home/b645/ibuild2/w32_ducati
-
-
+BASE_DIR=$HOME/ibuild2_env/w32_ducati
+PROD_DIR=$HOME/ibuild2_env/devr4tw32
 SPEC_NAME=spec_w32_DUCATI.conf
 
-
+# make ibuild2 build process(integrate your configs in ibuild2 to your base line)
+echo -e "\n\n\n>>>>>>>>>> INTEGRATE IBUILD2 CONFIG TO BASE LINE  <<<<<<<<<<"
 hb $BASE_DIR $PROD_DIR $SPEC_NAME $1 $2 $3 $4 $5 $6 $7 $8 $9
 
+pushd $BASE_DIR > /dev/null
+# create change list first(for that you can clean your base line quickly use script "restore_codebase.sh")
+echo -e "\n\n\n>>>>>>>>>> CREATE CHANGELIST FOR RESTORE(CLEAN) MAY NEED 6'MINUTE  <<<<<<<<<<"
+create_changelist.sh
+
 # build
-pushd $BASE_DIR
+echo -e "\n\n\n>>>>>>>>>> ANDROID FULL BUILD PROCESS  <<<<<<<<<<"
 source build/envsetup.sh
 lunch 19
 make flashfiles -j4
-REV_BASE=`svn info $BASE_DIR | grep Revision | awk -F' ' '{print $2}'`
-REV_IBUILD=`svn info $PROD_DIR | grep Revision | awk -F' ' '{print $2}'`
+REV_BASE=`svn info $BASE_DIR | grep Revision | awk -F' ' '{print $2}'` && echo $REV_BASE > $BASE_DIR/.svnversion
+REV_IBUILD=`svn info $PROD_DIR | grep Revision | awk -F' ' '{print $2}'` && echo $REV_IBUILD > $PROD_DIR/.svnversion
+echo -e "\n\n=========================================="
 date
-echo -e "  Base Version: $REV_BASE | $BASE_DIR"
-echo -e "IBUILD Version: $REV_IBUILD | $PROD_DIR"
-popd
+echo -e "  Base Version: $REV_BASE || $BASE_DIR"
+echo -e "IBUILD Version: $REV_IBUILD || $PROD_DIR"
+echo -e "==========================================\n\n"
+popd > /dev/null
