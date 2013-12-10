d45e615d01ab0edbc8e9651323516eb0c9f14524
b645 <b645@borqs.com>
2013-12-10 18:19:19 +0800

    New

--- bin/ducati.sh
+++ bin/ducati.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-BASE_DIR=$HOME/ibuild2_env/w32_ducati
+BINPATH=$HOME/ibuild2_env/bin
+
+BASE_DIR=$HOME/ibuild2_env/w32_ducati_v94
 PROD_DIR=$HOME/ibuild2_env/devr4tw32
 SPEC_NAME=spec_w32_DUCATI.conf
 
@@ -9,6 +11,12 @@ echo -e "\n\n\n>>>>>>>>>> INTEGRATE IBUILD2 CONFIG TO BASE LINE  <<<<<<<<<<"
 hb $BASE_DIR $PROD_DIR $SPEC_NAME $1 $2 $3 $4 $5 $6 $7 $8 $9
 if [ $? == 0 ];then 
 	pushd $BASE_DIR > /dev/null
+
+	# apply a patch for ignore .svn in findleaves.py/find/grep
+	alias find=find_ex
+	alias grep=grep_ex
+	patch -p0 < ${BINPATH}/findleaves_prune_svn.patch
+
 	# create change list first(for that you can clean your base line quickly use script "restore_codebase.sh")
 	echo -e "\n\n\n>>>>>>>>>> CREATE CHANGELIST FOR RESTORE(CLEAN) MAY NEED 6'MINUTE  <<<<<<<<<<"
 	create_changelist.sh
@@ -17,7 +25,8 @@ if [ $? == 0 ];then
 	echo -e "\n\n\n>>>>>>>>>> ANDROID FULL BUILD PROCESS  <<<<<<<<<<"
 	source build/envsetup.sh
 	lunch 19
-	make flashfiles -j4
+	START=`date`;make flashfiles -j2;echo $START;date
+	START=`date`;make bootimage -j2;echo $START;date
 	REV_BASE=`svn info $BASE_DIR | grep Revision | awk -F' ' '{print $2}'` && echo $REV_BASE > $BASE_DIR/.svnversion
 	REV_IBUILD=`svn info $PROD_DIR | grep Revision | awk -F' ' '{print $2}'` && echo $REV_IBUILD > $PROD_DIR/.svnversion
 	echo -e "\n\n=========================================="
